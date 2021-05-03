c70ef9204e3902b47c27bc6057276515b42a7c69
Mathieu Walravens <m.walravens@students.ephec.be>
2021-05-03 21:22:12 +0200

    Fix dollar

--- dns/entrypoint.sh
+++ dns/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export DOLLAR = "\$"
-
-envsubst < /etc/bind/db.template > /etc/bind/db.m1-2.ephec-ti.be
-envsubst < /etc/bind/named.conf.template > /etc/bind/named.conf
+DOLLAR="\$" envsubst < /etc/bind/db.template > /etc/bind/db.m1-2.ephec-ti.be
+DOLLAR="\$" envsubst < /etc/bind/named.conf.template > /etc/bind/named.conf
 /usr/sbin/named -g -c /etc/bind/named.conf -u bind
