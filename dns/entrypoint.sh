cf217b5b7d9f764e634df6db208c72bd9986590c
Mathieu Walravens <m.walravens@students.ephec.be>
2021-05-12 14:46:29 +0200

    Séparation zone interne et externes

--- dns/entrypoint.sh
+++ dns/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-DOLLAR="\$" envsubst < /etc/bind/db.template > /etc/bind/db.m1-2.ephec-ti.be
-DOLLAR="\$" envsubst < /etc/bind/named.conf.template > /etc/bind/named.conf
+for template in $(find /etc/bind/ -name "*.template"); do
+    file=${template%.*}
+    DOLLAR="\$" envsubst < $template > $file
+    echo ================================
+    echo $file
+    echo ================================
+done
+
 /usr/sbin/named -g -c /etc/bind/named.conf -u bind
