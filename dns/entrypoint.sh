91b44aa46bfa0af1773e159976523dbb13f987f5
Mathieu Walravens <m.walravens@students.ephec.be>
2021-05-12 15:40:44 +0200

    Echo file content at startup

--- dns/entrypoint.sh
+++ dns/entrypoint.sh
@@ -3,9 +3,11 @@
 for template in $(find /etc/bind/ -name "*.template"); do
     file=${template%.*}
     DOLLAR="\$" envsubst < $template > $file
-    echo ================================
-    echo $file
-    echo ================================
+    printf \n================================
+    echo    $file
+    printf ================================
+    cat $file
 done
+printf \n================================
 
 /usr/sbin/named -g -c /etc/bind/named.conf -u bind
